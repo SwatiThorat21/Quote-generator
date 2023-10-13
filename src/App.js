506427086c5018db7541bf8b4f903908a3540e4b
Swati <swatic019@gmail.com>
2023-10-13 15:43:05 +0530

    state updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
+function getRandomQuote(quotes) {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
 function App() {
+  const [quotesArr, setQuotesArr] = useState([]);
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuotesArr(data);
+        setQuote(data[0]);
+      });
+  }, []);
+
+  function getQuote() {
+    setQuote(getRandomQuote(quotesArr));
+  }
+ 
   return (
     <>
-    <div className="container">
-      <h1>Quote Generator</h1>
-      <section>
-        <button className="button-78">Get Quote</button>
-        <h3>
-          <span className='p-space'>"</span>
-          <p>You can observe a lot just by watching.</p>
-          <span>"</span>
-        </h3>
-        <i>- Yogi Berra, type.fit</i>
-      </section>
-    </div>
-    
+      <div className="container">
+        <h1>Quote Generator</h1>
+        <section>
+          <button className="button-78" onClick={getQuote}>
+            Get Quote
+          </button>
+          <h3>
+            <p>{quote?.text}</p>
+          </h3>
+          <i>- {quote?.author}</i>
+        </section>
+      </div>
     </>
   );
 }
